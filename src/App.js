7609b24a6d508bfb22aaad24d48ebfc516aa7c07
Will <w_priddle@hotmail.com>
2018-08-16 15:46:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>header</header>
+        <h1>Newsstack</h1>
+        <div>nav will go here</div>
+        <div>news component will go here</div>
+        <footer>footer</footer>
+      </div>
+    );
+  }
+}
+
+export default App;

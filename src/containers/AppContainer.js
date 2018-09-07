1090ea771781e0f1815da0ed8a3990b2cf211597
Will <w_priddle@hotmail.com>
2018-09-07 10:27:38 +0100

    Dynamically calling api based on button press.

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import NewsList from '../components/organisms/NewsList/NewsList';
 import styled from 'styled-components';
+import { get, isEqual } from 'lodash';
 
 const Header = styled.header`
   height: 80px;
@@ -29,33 +30,57 @@ const Menu = styled.nav`
   }
 `;
 
-class AppContainer extends Component {
-    state = {
-      news: [],
-      articles: []
+const newsStrings = {
+  "bbc": 'bbc-news',
+  "cnn": "cnn",
+  "financial times": "financial-times",
+  "independent": "independent",
+  "mtv": "mtv-news",
+  "national geographic": "national-geographic"
+}
+
+class AppContainer extends React.Component {
+  state = {
+    articles: [],
+    newsSelection: 'bbc-news'
   }
 
   componentDidMount() {
-      axios.get(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=22584ee7883646308b29d728c5895470`)
+    this.getNews('bbc-news');
+  }
+
+  getNews = (channel) => {
+    let newsUrl = `https://newsapi.org/v2/top-headlines?sources=${channel}&apiKey=22584ee7883646308b29d728c5895470`;
+      axios.get(newsUrl)
       .then(res => {
           const articles = res.data.articles;
           this.setState({ articles });
       })
   }
+  
+  changeChannel = (e) => {
+    console.log('change channel');
+    // console.log(e.target.id);
+    let newChannel = e.target.id;
+    this.getNews(newChannel);
+  }
+  
+
   render() {
+
     return (
       <div>
         <Header>header</Header>
         <Container>
+        <div>SELECTION: {this.state.newsSelection}</div>
           <Menu>
             <ul>
-              <li>bbc</li>
-              <li>blah</li>
-              <li>blah</li>
-              <li>blah</li>
-              <li>blah</li>
-              <li>blah</li>
-              <li>blah</li>
+              <li><button id="bbc-news" onClick={this.changeChannel}>bbc</button></li>
+              <li><button id="cnn" onClick={this.changeChannel}>cnn</button></li>
+              <li><button id="financial-times" onClick={this.changeChannel}>financial times</button></li>
+              <li><button id="independent" onClick={this.changeChannel}>independent</button></li>
+              <li><button id="mtv-news" onClick={this.changeChannel}>mtv</button></li>
+              <li><button id="national-geographic" onClick={this.changeChannel}>national geographic</button></li>
             </ul>
           </Menu>
           <NewsList articles={this.state.articles}/>
